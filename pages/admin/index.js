49fc5543dba0f305a98bbe44f2000b5a2f1563e2
ZEZE_COBRA <yeridsonruan995@gmail.com>
2024-05-18 16:37:37 -0300

    conclusão de autenticação da conta google

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,67 +1,50 @@
 'use client';
 
-import { getServerSession } from 'next-auth/next';
-import { authOptions } from '../api/auth/[...nextauth]';
-import { useState, useEffect } from 'react';
 import { getSession, signOut } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
-
-// export default function Page() {
-//   const [session, setSession] = useState();
-
-//   useEffect(() => {
-//     async function fetchData() {
-//       const session = await getServerSession(authOptions);
-//       setSession(session);
-//     }
-
-//     fetchData();
-
-//     return session;
-//   }, []);
-
-//   return (
-//     <>
-//       <h1>olalala </h1>
-//       <button onClick={()=>signOut()}>sair</button>
-//     </>
-//   );
-// }
+import { useEffect, useState } from 'react';
 
 export default function Page() {
-  const {push} = useRouter()
+  const { push, replace } = useRouter();
   const session = getSession();
-  const [account, setAcount] = useState([]);
+  const [account, setAccount] = useState([]);
+  const [expire, setExpire] = useState();
 
   async function dataUser() {
-    const response = await session.then((data) => data.user)
-    setAcount(response.forEach(element => {
-      element
-    }))
+    const data = (await session)?.user;
+    const response = (await session)?.expires;
 
-    console.log(account);
+    setAccount([0, data]);
+    setExpire([0, response]);
   }
 
-  if(!session){
-    push('/conta/login')
-  } else {
+  
+  useEffect(()=>{
+    if (!session?.user) {
+      replace('/conta/login')
+      return
+    }
     dataUser()
+  },[])
+
+  function get() {
+    dataUser();
+    console.log(account[1]);
   }
 
-  function handleGetSession() {
-  }
+  console.log(session);
 
   return (
     <>
-      <h1>TEla de login adm {account}</h1>
+      <h1>
+        TEla de login adm <b> {account[1]?.name} </b>
+      </h1>
 
-
-      <button
-        onClick={handleGetSession}
-        className='daisy-btn daisy-btn-warning'
-      >
-        Log out
-      </button>
+      <span>{account[1]?.email}</span>
+      <br />
+      <button onClick={() => signOut()}>Sign out</button>
+      <br/>
+      <button onClick={get}>usuario</button>
     </>
   );
 }
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -6,13 +6,6 @@ export const authOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      // profile(profile) {
-      //   return(
-      //       profile.email,
-      //       profile.name,
-      //       profile.picture
-      //   )
-      // },
     }),
   ],
   pages: {
