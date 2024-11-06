4bc574f9b36975a8bd3d0f5da0d91897ef34dd35
Yeridson Ruan Nascimento Teixeira <98131797+Kobra-Dev995@users.noreply.github.com>
2024-11-06 06:13:50 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -56,7 +56,7 @@ export default function LoginScreen(props) {
         }
 
         Cookie(inputEmailValue);
-        setIsLoading(false);
+        setIsLoading(true);
         push('/home');
       }
     });
